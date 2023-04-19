3ce99a445719ed167d6d6e1ecd4e9d32637ae4fa
hashtag-squirrel <alita2000@web.de>
2023-04-19 15:58:01 +0200

    Add function to fill diceArray with random values when roll dice is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,45 @@
+const rollBtn = document.getElementById('roll-button');
+
+let diceArray = [0, 0, 0, 0, 0];
+
+document.addEventListener('DOMContentLoaded', function() {
+    runGame();
+    rollBtn.addEventListener('click', function() {rollDice()});
+})
+
 /** 
 * Runs the game
 */ 
 function runGame() {
-
+    console.log("Game is running");
 }
 
 /**
 * Calculates the score for a picked field and returns the score
 */ 
 function calculateFieldScore() {
-
+    console.log("Calculating Field Score");
 }
 
 /** 
 * Calculates the total score whenever a field is filled
 */
 function calculateTotalScore() {
-
+    console.log("Calculating Total Score");
 }
 
 /**
 * Function to lock dice based on player's choice, returns locked dice
 */
-function lockDice() {
-
+function lockDice() {   
+    console.log("Locking Dice");
 }
 
 /**
 * Function to unlock dice based on player's choice, returns unlocked dice
 */
 function unlockDice() {
-
+    console.log("Unlocking Dice");
 }
 
 /**
@@ -38,19 +47,23 @@ function unlockDice() {
 * and indicates how many rolls left per turn
 */
 function rollDice() {
-
+    for (let dice in diceArray) {
+        diceArray[dice] = Math.floor(Math.random() * 6) + 1;
+    }
+    console.log(diceArray);
+    return diceArray;
 }
 
 /**
 * Lets the player pick a field for the turn and ends the turn
 */
 function endTurn() {
-
+    console.log("Ending Turn");
 }
 
 /**
 * Displays a rules popup
 */ 
 function displayRules() {
-
+    console.log("Displaying Rules");
 }
