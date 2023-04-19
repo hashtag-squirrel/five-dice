7413cb4776958fafb0174263fafd71f5d984efd0
hashtag-squirrel <alita2000@web.de>
2023-04-19 15:18:03 +0200

    Add empty functions for game logic, add turns to html and css

---
+++ assets/js/script.js
@@ -0,0 +1,56 @@
+/** 
+* Runs the game
+*/ 
+function runGame() {
+
+}
+
+/**
+* Calculates the score for a picked field and returns the score
+*/ 
+function calculateFieldScore() {
+
+}
+
+/** 
+* Calculates the total score whenever a field is filled
+*/
+function calculateTotalScore() {
+
+}
+
+/**
+* Function to lock dice based on player's choice, returns locked dice
+*/
+function lockDice() {
+
+}
+
+/**
+* Function to unlock dice based on player's choice, returns unlocked dice
+*/
+function unlockDice() {
+
+}
+
+/**
+* Generates random numbers between 1 and 6 for unlocked dice, returns the dice values
+* and indicates how many rolls left per turn
+*/
+function rollDice() {
+
+}
+
+/**
+* Lets the player pick a field for the turn and ends the turn
+*/
+function endTurn() {
+
+}
+
+/**
+* Displays a rules popup
+*/ 
+function displayRules() {
+
+}
